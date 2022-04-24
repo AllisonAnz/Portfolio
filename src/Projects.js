4e44bd363bd4eaf579d82fe857e7f152e87361e7
AllisonAnz <AllisonAnzalone@gmail.com>
2022-04-24 11:27:08 -0500

    Fix Project Cards

--- src/Projects.js
+++ src/Projects.js
@@ -30,7 +30,7 @@ export default function Projects({ data }) {
                 <Typography variant="h2" gutterBottom align="center" innerRef={animRef}>
                     Projects
                 </Typography>
-                <Hidden smDown>
+                <Hidden mdDown>
                     <Fade in={animate} style={{ transitionDelay: '250ms' }}>
                         <div>
                             <Image
